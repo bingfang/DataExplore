02f960b89a71cac3542f3e641bdc43f26d2f4eed
bingfang <bingfang@users.noreply.github.com>
2015-05-10 17:21:48 -0400

    Update project1.R

--- project1.R
+++ project1.R
@@ -7,22 +7,20 @@ hhp <- hhp[hhp$Date %in% c("1/2/2007", "2/2/2007"),]
 
 hhp$Date_Time <- paste(hhp$Date, hhp$Time)
 hhp$Date_Time <- strptime(hhp$Date_Time, "%d/%m/%Y %H:%M:%S")
+
 # Fig1
-with(hhp, hist(Global_active_power, main = "Global active power", xlab = "Global Active Power (kilowatts)", ylab = "Frequency", col = "red"))
+with(hhp, hist(Global_active_power, main = "Global active power", xlab = "Global active power (kilowatts)", ylab = "Frequency", col = "red"))
 #Fig2
 plot(x = hhp$Date_Time, y = hhp$Global_active_power, type = "l", xlab = "", ylab = "Global active power (kilowatts)")
-
 #Fig3
 plot(x = hhp$Date_Time, y = hhp$Sub_metering_1, type = "n", xlab = "", ylab = "Energy sub metering")
 lines(x = hhp$Date_Time, y = hhp$Sub_metering_1)
 lines(x = hhp$Date_Time, y = hhp$Sub_metering_2, col = "red")
 lines(x = hhp$Date_Time, y = hhp$Sub_metering_3, col = "blue")
 legend("topright", legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty = 1, col = c("black", "red", "blue"))
-
 #Fig 4
-
 par(mfcol = c(2, 2))
-plot(x = hhp$Date_Time, y = hhp$Global_active_power, type = "l", xlab = "", ylab = "Global Active Power")
+plot(x = hhp$Date_Time, y = hhp$Global_active_power, type = "l", xlab = "", ylab = "Global active power")
 
 plot(x = hhp$Date_Time, y = hhp$Sub_metering_1, type = "n", xlab = "", ylab = "Energy sub metering")
 lines(x = hhp$Date_Time, y = hhp$Sub_metering_1)
@@ -30,9 +28,7 @@ lines(x = hhp$Date_Time, y = hhp$Sub_metering_2, col = "red")
 lines(x = hhp$Date_Time, y = hhp$Sub_metering_3, col = "blue")
 legend("topright", legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty = 1, col = c("black", "red", "blue"))
 
-
 plot(x = hhp$Date_Time, y = hhp$Voltage, type = "l", xlab = "datetime", ylab = "Voltage")
-
 
 plot(x = hhp$Date_Time, y = hhp$Global_reactive_power, type = "l", xlab = "datetime", ylab = "Global Reactive Power")
 
